5d2681d8169b9b7f9bc7f6bde31d43ee4b6b01dc
Dhamith Hewamullage <hewamullage123@gmail.com>
2018-07-12 07:12:41 +0530

    sql backup

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ USE `EMS`;
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 04, 2018 at 03:02 AM
+-- Generation Time: Jul 12, 2018 at 01:41 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -17,6 +17,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `EMS`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ATTENDANCE`
+--
+
+CREATE TABLE `ATTENDANCE` (
+  `ID` int(11) NOT NULL,
+  `EMP_ID` varchar(10) NOT NULL,
+  `DATE_IN` date DEFAULT NULL,
+  `CLOCK_IN` time DEFAULT NULL,
+  `DATE_OUT` date DEFAULT NULL,
+  `CLOCK_OUT` time DEFAULT NULL,
+  `HOURS` decimal(10,2) DEFAULT NULL,
+  `OT` decimal(10,2) DEFAULT NULL,
+  `IS_CLOCKED_OUT` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ATTENDANCE`
+--
+
+INSERT INTO `ATTENDANCE` (`ID`, `EMP_ID`, `DATE_IN`, `CLOCK_IN`, `DATE_OUT`, `CLOCK_OUT`, `HOURS`, `OT`, `IS_CLOCKED_OUT`) VALUES
+(6, 'E555', '2018-07-11', '03:51:00', '2018-07-11', '03:53:00', '0.02', '0.00', 1),
+(7, 'E555', '2018-07-02', '03:03:03', '2018-07-02', '03:04:03', '1.00', '0.00', 1),
+(8, 'E555', '2018-07-04', '03:03:03', '2018-07-04', '03:04:03', '1.00', '0.00', 1),
+(9, 'E555', '2018-06-01', '03:03:03', '2018-06-01', '03:04:03', '1.00', '0.00', 1);
 
 -- --------------------------------------------------------
 
@@ -35,10 +63,11 @@ CREATE TABLE `DEPARTMENT` (
 --
 
 INSERT INTO `DEPARTMENT` (`ID`, `NAME`, `MANAGER_ID`) VALUES
-(1, 'IT', 'E338'),
+(1, 'IT', 'E09321'),
 (2, 'HR', 'E545'),
 (3, 'SALES', 'E303'),
-(4, 'ACCOUNTING', 'E223');
+(4, 'ACCOUNTING', 'E223'),
+(6, 'Marketing', 'E338');
 
 -- --------------------------------------------------------
 
@@ -70,14 +99,14 @@ CREATE TABLE `EMPLOYEE` (
 --
 
 INSERT INTO `EMPLOYEE` (`ID`, `FIRST_NAME`, `LAST_NAME`, `DOB`, `GENDER`, `NIC`, `EMP_ID`, `SALARY`, `JOINED_DATE`, `MANAGED_BY`, `DEPARTMENT`, `POSITION`, `ADDRESS_LINE_1`, `ADDRESS_LINE_2`, `CITY`, `TELEPHONE_NO`) VALUES
-(1, 'DHAMITH', 'HEWAMULLAGE', '1993-01-01', 'MALE', '0123456789V', 'E338', '9999.99', '2012-01-01', NULL, 1, 'DEV', '31', 'SRI ANANDARAMA MAWATHA', 'KOLONNAWA', '0778675309'),
-(2, 'JOHN', 'DOE', '1989-01-01', 'MALE', '0123456789V', 'E556', '99999.99', '2015-01-01', NULL, 1, 'OPS', '1, CURIOSITY STREET', NULL, 'MARS', '0123456789'),
-(3, 'JANE', 'DOE', '1979-01-01', 'FEMALE', '0123456789V', 'E223', '99999.99', '2016-01-01', NULL, 4, NULL, '1, CURIOSITY STREET', NULL, 'MARS', '0123456789'),
-(4, 'MIKE', 'HUNT', '1969-01-01', 'MALE', '0123456789V', 'E303', '99999.99', '2017-01-01', NULL, 3, NULL, '666, SOME STREET', 'SOME OTHER STREET', 'SWING CITY', '0123456789'),
+(1, 'DHAMITH', 'HEWAMULLAGE', '1993-01-01', 'male', '0123456789V', 'E338', '9999.99', '2012-01-01', 1, 1, 'DEV', '31', 'SRI ANANDARAMA MAWATHA', 'KOLONNAWA', '0778675309'),
+(2, 'JOHN', 'DOE', '1989-01-01', 'male', '0123456789V', 'E556', '99999.99', '2015-01-01', 1, 1, 'OPS', '1, CURIOSITY STREET', '', 'MARS', '0123456789'),
+(3, 'JANE', 'DOE', '1979-01-01', 'female', '0123456789V', 'E223', '99999.99', '2018-02-01', 5, 2, 'HR', '1, CURIOSITY STREET', '', 'MARS', '0123456789'),
+(4, 'MIKE', 'HUNT', '1969-01-01', 'male', '0123456789V', 'E303', '99999.99', '2017-01-01', 4, 3, '', '666, SOME STREET', 'SOME OTHER STREET', 'SWING CITY', '0123456789'),
 (5, 'ROBERT', 'SOLE', '1959-01-01', 'MALE', '0123456789V', 'E545', '99999.99', '2018-01-01', NULL, 2, NULL, '111, SOME STREET', 'SOME ANOTHER STREET', 'FUCK CITY', '0123456789'),
-(6, 'Test', 'Tester', '2018-07-19', 'male', '123123v', 'E666', '999999.99', '2018-07-20', 3, 4, 'Auditor', '117/3, Rahula road', 'Sugathi Mawatha', 'Angoda', '+94773630792'),
-(7, 'New', 'Test', '2018-07-20', 'male', '123123', 'E888', '123121323.00', '2018-07-18', 5, 2, 'HR', '117/3, Rahula road', 'Sugathi Mawatha', 'Angoda', '+94773630792'),
-(8, 'Login', 'Test', '2018-05-11', 'male', '1213', 'E555', '123123.00', '2018-06-07', 5, 2, 'Sales', '117/3, Rahula road', 'Sugathi Mawatha', 'Angoda', '+94773630792');
+(8, 'Login', 'Test', '2018-05-11', 'male', '1213', 'E555', '999999.50', '2018-06-07', 5, 2, 'Sales', '117/3, Rahula road', 'Sugathi Mawatha', 'Angoda', '+94773630792'),
+(25, 'Test', 'Name', '1980-10-28', 'male', '0123456789v', 'E441', '32563.59', '2017-09-14', 4, 3, 'Sales', '111, some place', 'some street', 'some city', '01234567890'),
+(27, 'Test', 'Person', '1990-03-03', 'female', '123467985', 'E09321', '12564321.00', '2015-02-02', 1, 1, 'DEV', '123, Some Place', 'Some street', 'Some city', '1234453');
 
 -- --------------------------------------------------------
 
@@ -104,7 +133,14 @@ CREATE TABLE `LEAVES` (
 --
 
 INSERT INTO `LEAVES` (`ID`, `EMP_ID`, `MANAGER`, `REASON`, `TYPE`, `START_ON_DATE`, `END_ON_DATE`, `START_ON_TIME`, `END_ON_TIME`, `IS_APPROVED`, `IS_CONFIRMED`) VALUES
-(1, 'E555', 5, 'test', 'annual', '2018-07-05', '2018-07-12', '01:00', '01:00', 0, 0);
+(4, 'E555', 5, 'test', 'short', '2018-07-06', '2018-07-06', '01:00', '02:00', 0, 0),
+(5, 'E555', 5, 'test', 'short', '2018-07-11', '2018-07-11', '01:00', '02:00', 1, 1),
+(6, 'E555', 5, 'test', 'annual', '2018-07-19', '2018-07-25', '', '', 1, 1),
+(7, 'E555', 5, 'Some reason here', 'casual', '2018-07-09', '2018-07-10', '', '', 0, 0),
+(8, 'E555', 5, 'test', 'annual', '2018-07-01', '2018-07-04', '', '', 1, 1),
+(9, 'E555', 5, 'c test', 'casual', '2018-07-01', '2018-07-04', '', '', 1, 0),
+(10, 'E555', 5, 'test', 'short', '2018-07-11', '2018-07-11', '01:00', '05:00', 0, 0),
+(11, 'E555', 5, 'test reason', 'casual', '2018-01-01', '2018-01-05', '', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -117,7 +153,7 @@ CREATE TABLE `LEAVES_LEFT` (
   `EMP_ID` varchar(10) NOT NULL,
   `ANNUAL` decimal(10,0) NOT NULL,
   `CASUAL` decimal(10,0) NOT NULL,
-  `SHORT` int(11) NOT NULL
+  `SHORT` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -125,7 +161,9 @@ CREATE TABLE `LEAVES_LEFT` (
 --
 
 INSERT INTO `LEAVES_LEFT` (`ID`, `EMP_ID`, `ANNUAL`, `CASUAL`, `SHORT`) VALUES
-(1, 'E555', '4', '7', 2);
+(1, 'E555', '10', '7', '2'),
+(4, 'E441', '10', '7', '2'),
+(6, 'E09321', '10', '7', '2');
 
 -- --------------------------------------------------------
 
@@ -144,9 +182,11 @@ CREATE TABLE `LOGIN` (
 --
 
 INSERT INTO `LOGIN` (`ID`, `EMP_ID`, `PASSWORD`) VALUES
-(1, 'E666', '1'),
-(2, 'E888', '1'),
-(3, 'E555', '$2a$10$Frv43oAjOYbEeOi6DtlaSuIAqAME8YN6bjBp/bF/Q5ucILDCbUM6.');
+(3, 'E555', '$2a$10$Frv43oAjOYbEeOi6DtlaSuIAqAME8YN6bjBp/bF/Q5ucILDCbUM6.'),
+(4, 'E223', '$2a$10$IqB0VrRcN0yQDkypoFoOU.HEqYOi4QEbyCqQhdGIKm.ff2SgpmUo2'),
+(5, 'E338', '$2a$10$dQ8cJrRfkZE0am.QhREnAOAPteTmhDje6PY3Tl/iMkWvzEmB2LIT.'),
+(8, 'E441', '$2a$10$4/qu2X64wx2cw0U83nB1GuN6mYVDUPqecThXV0Hb72yy8io80acDu'),
+(10, 'E09321', '$2a$10$IOE8dHsPsFHz2JWgtxMIc.3eCt9C05vEdH1gK4rKj4vSJXvv7oBDy');
 
 -- --------------------------------------------------------
 
@@ -164,6 +204,14 @@ CREATE TABLE `TASKS` (
   `IS_COMPLETED` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `TASKS`
+--
+
+INSERT INTO `TASKS` (`ID`, `DEPT_ID`, `NAME`, `DESC`, `START_ON`, `END_ON`, `IS_COMPLETED`) VALUES
+(1, 2, 'Test Task', 'This is a test task. ', '2018-06-30', '2018-07-21', 0),
+(2, 2, 'Test task 2', 'This is a test task', '2018-07-03', '2018-07-06', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -177,6 +225,13 @@ CREATE TABLE `TASK_ASSESSMENT` (
   `PERFORMANCE` double DEFAULT NULL,
   `NOTES` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `TASK_ASSESSMENT`
+--
+
+INSERT INTO `TASK_ASSESSMENT` (`ID`, `TASK_ID`, `EMP_ID`, `PERFORMANCE`, `NOTES`) VALUES
+(1, 2, 'E555', 99.99, 'This is a test note');
 
 -- --------------------------------------------------------
 
@@ -192,8 +247,23 @@ CREATE TABLE `TASK_ASSIGNMENT` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `TASK_ASSIGNMENT`
+--
+
+INSERT INTO `TASK_ASSIGNMENT` (`ID`, `EMP_ID`, `TASK_ID`, `PROGRESS`) VALUES
+(1, 'E555', 1, '50'),
+(2, 'E555', 2, '100');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `ATTENDANCE`
+--
+ALTER TABLE `ATTENDANCE`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_emp_id_attnd` (`EMP_ID`);
 
 --
 -- Indexes for table `DEPARTMENT`
@@ -261,56 +331,68 @@ ALTER TABLE `TASK_ASSIGNMENT`
 --
 
 --
+-- AUTO_INCREMENT for table `ATTENDANCE`
+--
+ALTER TABLE `ATTENDANCE`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `DEPARTMENT`
 --
 ALTER TABLE `DEPARTMENT`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `EMPLOYEE`
 --
 ALTER TABLE `EMPLOYEE`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `LEAVES`
 --
 ALTER TABLE `LEAVES`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `LEAVES_LEFT`
 --
 ALTER TABLE `LEAVES_LEFT`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `LOGIN`
 --
 ALTER TABLE `LOGIN`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `TASKS`
 --
 ALTER TABLE `TASKS`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `TASK_ASSESSMENT`
 --
 ALTER TABLE `TASK_ASSESSMENT`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `TASK_ASSIGNMENT`
 --
 ALTER TABLE `TASK_ASSIGNMENT`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `ATTENDANCE`
+--
+ALTER TABLE `ATTENDANCE`
+  ADD CONSTRAINT `fk_emp_id_attnd` FOREIGN KEY (`EMP_ID`) REFERENCES `EMPLOYEE` (`EMP_ID`);
 
 --
 -- Constraints for table `DEPARTMENT`
@@ -363,4 +445,5 @@ ALTER TABLE `TASK_ASSESSMENT`
 ALTER TABLE `TASK_ASSIGNMENT`
   ADD CONSTRAINT `task_assignment_ibfk_1` FOREIGN KEY (`TASK_ID`) REFERENCES `TASKS` (`ID`),
   ADD CONSTRAINT `task_assignment_ibfk_2` FOREIGN KEY (`EMP_ID`) REFERENCES `EMPLOYEE` (`EMP_ID`);
+
 
